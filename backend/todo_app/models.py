fc20f10ad58c421f92bc0f33424476c627d67148
mrToad <korostylev.ov@gmail.com>
2022-03-15 22:59:21 +0300

    lesson3

---
+++ backend/todo_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from users.models import User
+
+class Project(models.Model):
+    name = models.CharField(max_length=128)
+    ref = models.URLField()
+    users = models.ManyToManyField(User)
+
+class TODO(models.Model):
+    project = models.ForeignKey(User, related_name='_project', on_delete=models.PROTECT)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, related_name='_user', on_delete=models.PROTECT)
+    is_active = models.BooleanField(default=True)
