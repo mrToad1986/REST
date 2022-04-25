60aff2b729157695d89ae770e9c8b88457c3e2be
mrToad <korostylev.ov@gmail.com>
2022-04-25 23:31:10 +0300

    lesson11

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -10,7 +10,8 @@ import TODOlist from "./components/TODOlist.js";
 import UserProjectList from "./components/UserProjectList.js";
 import LoginForm from "./components/LoginForm.js";
 import Cookies from 'universal-cookie';
-import {HashRouter, BrowserRouter, Route, Routes, Link, useLocation} from "react-router-dom";
+import {HashRouter, BrowserRouter, Route, Routes, Link, useLocation, Switch, Redirect} from "react-router-dom";
+import ProjectForm from "./components/ProjectForm";
 
 const NotFound = () => {
     let location = useLocation()
@@ -121,41 +122,85 @@ class App extends React.Component {
         }, this.getData)
     }
 
-
-    render() {
-        return (
-            <div>
-                <Menu/>
-                <BrowserRouter>
-                    <nav>
-                        <li>
-                            <Link to='/users'>Users</Link>
-                        </li>
-                        <li>
-                            <Link to='/project'>Projects</Link>
-                        </li>
-                        <li>
-                            <Link to='/todo'>TODOs</Link>
-                        </li>
-                        <li>
-                            {this.isAuth() ? <button onClick={() => this.logout()}>Logout</button> :
-                                <Link to='/login'>Login</Link>}
-                        </li>
-                    </nav>
-                    <Routes>
-                        <Route exact path='/users' element={<UserList users={this.state.users}/>}/>
-                        <Route exact path='/project' element={<ProjectList projects={this.state.projects}/>}/>
-                        <Route exact path='/todo' element={<TODOlist TODOs={this.state.TODOs}/>}/>
-                        <Route exact path='/login'
-                               element={<LoginForm getToken={(login, password) => this.getToken(login, password)}/>}/>
-                        <Route path='/user/:id' element={<UserProjectList users={this.state.users}/>}/>
-                        <Route path='*' element={<NotFound/>}/>
-                    </Routes>
-                </BrowserRouter>
-                <Footer/>
-            </div>
-        )
+    deleteProject(id) {
+        const headers = this.get_headers()
+        axios
+            .delete('http://127.0.0.1:8000/api/project/${id}', {headers})
+            .then(response => {
+                this.setState({projects: this.state.projects.filter((project) => project.id !== id)})
+            }).catch(error => console.log(error))
     }
-}
 
-export default App;
+    createProject(name, users) {
+        const headers = this.get_headers()
+        const data = {name: name, users: users}
+        axios.post(`http://127.0.0.1:8000/api/project/`, data, {headers})
+            .then(response => {
+                let new_project = response.data
+                const users = this.state.users.filter((item) => item.id ===
+                    new_project.users)[0]
+                new_project.users = users
+                this.setState({projects: [...this.state.projects, new_project]})
+            }).catch(error => console.log(error))
+
+        deleteTodo(id)
+        {
+            const headers = this.get_headers()
+            axios
+                .delete('http://127.0.0.1:8000/api/todo/${id}', {headers})
+                .then(response => {
+                    this.setState({projects: this.state.todo.filter((todo) => todo.id !== id)})
+                }).catch(error => console.log(error))
+        }
+
+
+        render()
+        {
+            return (
+                <div>
+                    <Menu/>
+                    <BrowserRouter>
+                        <nav>
+                            <li>
+                                <Link to='/users'>Users</Link>
+                            </li>
+                            <li>
+                                <Link to='/project'>Projects</Link>
+                            </li>
+                            <li>
+                                <Link to='/todo'>TODOs</Link>
+                            </li>
+                            <li>
+                                {this.isAuth() ? <button onClick={() => this.logout()}>Logout</button> :
+                                    <Link to='/login'>Login</Link>}
+                            </li>
+                        </nav>
+                        <Routes>
+                            <Route exact path='/users' element={<UserList users={this.state.users}/>}/>
+                            <Route exact path='/project' element={<ProjectList projects={this.state.projects}/>}/>
+                            <Route exact path='/todo' element={<TODOlist TODOs={this.state.TODOs}/>}/>
+                            <Route exact path='/login'
+                                   element={<LoginForm
+                                       getToken={(login, password) => this.getToken(login, password)}/>}/>
+                            <Route path='/user/:id' element={<UserProjectList users={this.state.users}/>}/>
+                            <Route path='*' element={<NotFound/>}/>
+                            <Route exact path='/project' component={() => <ProjectList projects={this.state.projects}
+                                                                                       deleteProjects={(id) => this.deleteProject(id)}/>}/>
+                            <Route exact path='/todo' component={() => <TODOlist TODOs={this.state.TODOs}
+                                                                                 deleteToDo={(id) => this.deleteTodo(id)}/>}/>
+                            <Route exact path='/project/create' component={() => <ProjectForm/>}/>
+                            <Route exact path='/project' component={() => <ProjectList items={this.state.projects}
+                                                                                       deleteProject={(id) => this.deleteBook(id)}/>}/>
+                            <Route exact path='/project/create' component={() => <ProjectForm
+                                createProject={(name, users) => this.createProject(name, users)}/>}/>
+                        </Routes>
+                    </BrowserRouter>
+                    <Footer/>
+                </div>
+            )
+        }
+    }
+
+    export
+    default
+    App;
